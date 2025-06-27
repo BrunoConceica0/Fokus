53784995c4b51802bfcffb0c69ca0679d31873f0
Bruno <bd687353@gmail.com>
2025-06-27 15:56:04 -0300

    Corrigindo as imagens

--- app/components/form/formClean.js
+++ app/components/form/formClean.js
@@ -1,6 +1,5 @@
 export default (texteraTask) => {
   const btnClean = document.querySelector(".app__form-footer__button--cancel");
-  console.log(btnClean);
   btnClean.addEventListener("click", (event) => {
     event.preventDefault();
     !texteraTask ? alert("Nenhum campo para limpar") : (texteraTask.value = "");
